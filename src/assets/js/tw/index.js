6011b73c2a619b81e899342ecf05fbe02d24f04d
dlrpfl <dlrpfl@ona.co.kr>
2023-06-13 17:36:20 +0900

    대만 비디오

--- src/assets/js/tw/index.js
+++ src/assets/js/tw/index.js
@@ -2,19 +2,16 @@ $(document).ready(function () {
 
   $(window).scroll(function() {
 
-		if ($(window).scrollTop() >= $('.lgmembersdays #box_content').offset().top - 80) {
+		if ($(window).scrollTop() >= $('.lgmembersdays .main').offset().top) {
 			$('.lgmembersdays .tab__panel--he').find('video').get(0).play();
 		}
 	});
 
-	if ($(window).scrollTop() >= $('.lgmembersdays #box_content').offset().top - 80) {
+	if ($(window).scrollTop() >= $('.lgmembersdays .main').offset().top) {
 		$('.lgmembersdays .tab__panel--he').find('video').get(0).play();
 	}
 
 	$('.lgmembersdays .tab__button').on('click', function() {
-
-		var target = '#' + $(this).attr('href');
-
 		$(this).closest('.tab').find('.tab__panel').find('video').get(0).pause();
 		$($(this).attr("href")).find('video').get(0).play();
 	});
