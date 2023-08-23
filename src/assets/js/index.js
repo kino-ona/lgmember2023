128e4c9384b7defc248182e87c214e5bece74bb0
dlrpfl <dlrpfl@ona.co.kr>
2023-08-23 16:16:53 +0900

    pc 완료

---
+++ src/assets/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.lgmembersdays .tab__button').on('click', function() {
+		var idx = $(this).index();
+		$(this).addClass('tab__button--selected').siblings('.tab__button').removeClass('tab__button--selected');
+		$(this).closest('.tab').find('.tab__panel').eq(idx).addClass('tab__panel--opened').siblings('.tab__panel').removeClass('tab__panel--opened');
+		$(this).closest('.tab').find('.tab__panel').eq(idx).find('.product__list').slick('refresh');
+	});
+
+	$('.lgmembersdays .product__list--slick').slick({
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+
+	$('.lgmembersdays .product__list--slick-row').slick({
+		infinite: false,
+		slidesToShow: 4,
+    slidesToScroll: 4,
+    rows: 2,
+	});
+});
